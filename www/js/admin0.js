adbc4daef1603cc9b4d619b67cdb61ea97cdee21
tdelbecque <git@sodad.com>
2017-12-13 12:02:52 +0100

    major refactoring

--- www/js/admin0.js
+++ www/js/admin0.js
@@ -5,41 +5,88 @@
  */
 
 SoDAD.Admin = {};
-SoDAD.Admin.loadUsers = function (d) {
-    var containerId = d.users_container_id;
-    var whenUpdateRow = function (dataRow) {
-	var options = $.extend ({query: "updateuser"}, dataRow.values);
+
+SoDAD.Admin.whenUpdateRowGen = function (queryUpdate, queryRefresh) {
+    return function (dataRow, callback) {
+	var options = $.extend ({query: queryUpdate}, dataRow.values);
 	$.post ("index.php",
 		options,
-		function (data) {
-		    console.log (JSON.stringify (data))
+		function (updateRetData) {
+		    if (SoDAD.isDefined (callback) && $.isFunction (callback)) {
+			$.post ("index.php",
+				{query: queryRefresh},
+				function (data) {
+				    if (SoDAD.isDefined (data.error))
+					alert ("error");
+				    else 
+					callback (data, dataRow);
+				},
+				'json')
+		    }
 		},
+		'json');
+    }
+};
+
+SoDAD.Admin.newElementGen = function (queryNew) {
+    return function (fields) {
+	$.post ("index.php",
+		{query: queryNew},
+		function (ne) {
+		    $.each (fields,
+			    function (_, field) {
+				if (field.inputId) {
+				    if (SoDAD.isDefined (ne [field.name]))
+					$("#" + field.inputId).val (ne [field.name]);
+				    else
+					$("#" + field.inputId).val ("");					
+				}});	
+		},
+		'json');
+    }
+};
+
+SoDAD.Admin.loadUsers = function (d) {
+    var containerId = d.users_container_id;
+    var whenUpdateRow = SoDAD.Admin.whenUpdateRowGen ("updateusers", "allusers");
+    var whenAddElement = function (dataRow, callback) {
+	var options = $.extend ({query: "adduser"}, dataRow.values);
+	$post ("index.php",
+	       options,
+	       function (data) {
+		   if (SoDAD.isDefined (callback) && $.isFunction (callback))
+		       callback (data, dataRow);
+	       },
 	       'json');
     };
     
-    $.getJSON ("index.php",
-	       {query: "allusers"},
-	       function (data) {
-		   var options = {
-		       newElement: d.users.newElement,
-		       whenUpdateRow: whenUpdateRow,
-		       containerId: containerId,
-		       tableId: containerId + "-table",
-		       editForm: {
-			   title: "Administration des utilisateurs",
-			   containerId: containerId + "-edit-modal"}};
-		   
-		   var x = new SoDAD_HTMLTable (data, options);
-
-		   $("#" + containerId)
-		       .empty ()
-		       .append (x.tableElement)
-		       .append (x.editElement);
-	       })
+    $.post ("index.php",
+	    {query: "allusers"},
+	    function (data) {
+		var options = {
+		    newElement: d.users.newElement,   // new elements create a blank data
+		    whenUpdateRow: whenUpdateRow,
+		    whenAddElement: whenAddElement,
+		    containerId: containerId,
+		    tableId: containerId + "-table",
+		    editForm: {
+			title: "Administration des utilisateurs",
+			containerId: containerId + "-edit-modal"}};
+		
+		var x = new SoDAD_HTMLTable (data, options);
+		
+		$("#" + containerId)
+		    .empty ()
+		    .append (x.tableElement)
+		    .append (x.editElement);
+	    },
+	   'json')
 };
 
 SoDAD.Admin.loadStructures = function (d) {
     var containerId = d.structures_container_id;
+    var whenUpdateRow = SoDAD.Admin.whenUpdateRowGen ("updatestructure", "allstructures");
+    /*
     var whenUpdateRow = function (dataRow) {
 	var options = $.extend ({query: "updatestructure"}, dataRow.values);
 	$.post ("index.php",
@@ -49,44 +96,81 @@ SoDAD.Admin.loadStructures = function (d) {
 		},
 	       'json');
     };
-    
-    $.getJSON ("index.php",
-	       {query: "allstructures"},
+    */
+    var whenAddElement = function (dataRow) {
+	var options = $.extend ({query: "addstructures"}, dataRow.values);
+	$post ("index.php",
+	       options,
 	       function (data) {
-		   var options = {
-		       whenUpdateRow: whenUpdateRow,
-		       containerId: containerId,
-		       tableId: containerId + "-table",
-		       editForm: {
-			   title: "Administration des structures",
-			   containerId: containerId + "-edit-modal"}};
-		   
-		   var x = new SoDAD_HTMLTable (data, options);
-
-		   $("#" + containerId)
-		       .empty ()
-		       .append (x.tableElement)
-		       .append (x.editElement);
-	       })
+	       },
+	       'json');
+    };
+    $.post ("index.php",
+	    {query: "allstructures"},
+	    function (data) {
+		var options = {
+		    whenUpdateRow: whenUpdateRow,
+		    whenAddElement: whenAddElement,
+		    containerId: containerId,
+		    tableId: containerId + "-table",
+		    editForm: {
+			title: "Administration des structures",
+			containerId: containerId + "-edit-modal"}};
+		
+		var x = new SoDAD_HTMLTable (data, options);
+		
+		$("#" + containerId)
+		    .empty ()
+		    .append (x.tableElement)
+		    .append (x.editElement);
+	    },
+	   'json')
 };
+
+SoDAD.Admin.load = function (what, uid, formTitle) {
+    var containerId = what + "-table";
+    var whenUpdateRow = SoDAD.Admin.whenUpdateRowGen ("update" + what, "all" + what);
+    var whenAddElement = function (dataRow, callback) {
+	var options = $.extend ({query: "add" + what}, dataRow.values);
+	$post ("index.php",
+	       options,
+	       function (data) {
+		   if (SoDAD.isDefined (callback) && $.isFunction (callback))
+		       callback (data, dataRow);
+	       },
+	       'json');
+    };
+    
+    $.post ("index.php",
+	    {query: "all" + what},
+	    function (data) {
+		if (SoDAD.isDefined (data.error)) {
+		    alert (data.error);
+		} else {
+		    var options = {
+			newElement: SoDAD.Admin.newElementGen ("new" + what),
+			whenUpdateRow: whenUpdateRow,
+			whenAddElement: whenAddElement,
+			containerId: containerId,
+			tableId: containerId + "-table",
+			editForm: {
+			    title: formTitle,
+			    containerId: containerId + "-edit-modal"}};
+		    
+		    var x = new SoDAD_HTMLTable (data, options);
+		    
+		    $("#" + containerId)
+			.empty ()
+			.append (x.tableElement)
+			.append (x.editElement);
+		}
+	    },
+	   'json')
+}
 
 SoDAD.pageAdmin0WhenLoaded = function (uid) {
     var usersConfig = {
-	newElement: function (fields) {
-	    $.post ("index.php",
-		    {query: "newuser"},
-		    function (ne) {
-			$.each (fields,
-			    function (_, field) {
-				if (field.inputId) {
-				    if (SoDAD.isDefined (ne [field.name]))
-					$("#" + field.inputId).val (ne [field.name]);
-				    else
-					$("#" + field.inputId).val ("");					
-				}});	
-		    },
-		    'json');
-	},
+	newElement: SoDAD.Admin.newElementGen ("newusers"),
 	containerId: "users-table"
     };
     
@@ -97,6 +181,8 @@ SoDAD.pageAdmin0WhenLoaded = function (uid) {
         structures_container_id: "structures-table"
     };
 
-    SoDAD.Admin.loadUsers (config);
-    SoDAD.Admin.loadStructures (config);
+    //    SoDAD.Admin.loadUsers (config);
+    SoDAD.Admin.load ("users", uid, "Administration des utilisateurs");
+    //SoDAD.Admin.loadStructures (config);
+    SoDAD.Admin.load ("structures", uid, "Administration des structures");
 }
--- www/js/table.js
+++ www/js/table.js
@@ -46,16 +46,19 @@ function SoDAD_HTMLTable (data, options) {
 
     this.updateRow = function (dataRow) {
 	if (options.whenUpdateRow)
-	    options.whenUpdateRow (dataRow);
-	this.updateRowView (dataRow);
+	    options.whenUpdateRow (dataRow,
+				   function (data, dataRow) {
+				       self.data = data;
+				       self.refreshView ();
+				   });
+//	this.refreshView ();
     }
 }
 
 $.extend (SoDAD_HTMLTable.prototype, {
     updateRowView: function (dataRow) {
 	var fields = this.data.fields;
-	
-	$("#" + this.options.tableId + " tr[data-row=" + dataRow.id + "] td[data-fname]").each (
+	$("#" + this.options.tableId + " tr[data-key=" + dataRow.values[this.data.key] + "] td[data-fname]").each (
 	    function (_, x) {
 		var e = $(x);
 		var fname = e.attr ("data-fname");
@@ -77,6 +80,117 @@ $.extend (SoDAD_HTMLTable.prototype, {
 	);
     },
 
+    refreshView: function () {
+	var self = this;
+	var undef;
+	var forKey = function (data, key) {
+	    var r = $.grep (data.rows,
+			    function (r, _) {
+				return r.values [data.key] === key;
+			    });
+	    return r.length > 0 ? r [0] : undef;
+	};
+    
+	var options = this.options;
+	var data = this.data;
+	$.each ($("#" + options.containerId + " tr[data-key]"),
+		function (_, rowElt) {
+		    var dataRow = forKey (data, rowElt.dataset.key);
+		    if (dataRow) {
+			self.updateRowView (dataRow);
+			dataRow.touched = true;
+		    } else {
+			$(rowElt).remove ();
+		    }
+		});
+	$.each (data.rows,
+		function (_, dataRow) {
+		    if (dataRow.touched) {
+			delete dataRow.touched;
+		    } else {
+			self.createRowElement (dataRow)
+			    .appendTo ($("#" + options.tableId + "-tbody"));
+		    }});
+    },
+
+ /*
+  *  Creates the edit button for a row
+  */   
+    createRowEditBtnElement: function (dataRow) {
+	var self = this;
+	return createGlyph ("edit",
+			    {
+				visible: dataRow.editable
+			    },
+			    {
+				"data-toggle": "modal",
+				"data-target": '#' + this.options.editForm.containerId
+			    })
+	    .click ((function (dataRow) {  // to create a closure
+		return function () {
+		    var newDataRow = {values: $.extend ({}, dataRow.values)};
+		    var f = $('#' + self.options.editForm.containerId);
+		    var form = $("#" + self.options.editForm.containerId + "-form");
+		    form.off ("submit");
+		    form.submit (function (e) {
+			$.each (self.data.fields,
+				function (_, field) {
+				    if (field.inputId && ! field.noneditable && ! self.options.noneditable) {
+					newDataRow.values [field.name] = $("#" + field.inputId).val ();
+				    }
+				});
+			self.updateRow (newDataRow);
+			e.preventDefault ();
+			$("#" + self.options.editForm.containerId)
+			    .modal ('hide');
+			return false;
+		    });
+		    $.each (self.data.fields,
+			    function (_, field) {
+				if (field.inputId) {
+				    var value = dataRow.values [field.name];
+				    var options = dataRow.options [field.name];
+				    var nonEditable = field.noneditable || dataRow.options [field.name].noneditable;
+				    var e = $("#" + field.inputId);
+				    e[0].disabled = nonEditable;
+				    e.val (value);
+				}})}})(dataRow))
+    },
+
+    createRowElement: function (dataRow) {
+	var self = this;
+	var row = $("<tr/>",
+		    {"data-row": dataRow.id,
+		     "data-key": dataRow.values [this.data.key]
+		    });
+	var c = $ ("<td/>").appendTo (row);
+
+	this.createRowEditBtnElement (dataRow).appendTo (c);
+	    
+	createGlyph ("remove",
+		     {
+			 visible: dataRow.deletable
+		     })
+	    .appendTo (c);
+	var I = this.data.I;
+	for (i = 0; i < I.length; i ++) {
+	    var field = this.data.fields [I[i]];
+	    var fname = field.name;
+	    var label;
+	    if (field.type.indexOf ('select') === 0) {
+		var o = $.grep (field.options,
+				function (o,_) {
+				    return o.value === dataRow.values [fname];
+				})[0];
+		label = o ? o.label : '?';
+	    } else {
+		label = dataRow.values [fname];
+	    }
+	    
+	    $("<td/>", {"data-fname": fname}).appendTo (row).text (label);
+	}
+	return row;
+    },
     
     toHTMLTable: function () {
 	var self = this;
@@ -101,10 +215,11 @@ $.extend (SoDAD_HTMLTable.prototype, {
 				    dataRow.values [field.name] = $("#" + field.inputId).val ();
 				}
 			    });
-		    //self.updateRow (dataRow);
+		    
 		    e.preventDefault ();
 		    $("#" + options.editForm.containerId)
 			.modal ('hide');
+		    self.refreshView ();
 		    return false;
 		});
 		    
@@ -128,89 +243,26 @@ $.extend (SoDAD_HTMLTable.prototype, {
 		
 	    });
 	
-	var table = $('<table/>', {id: options.containerId + "-table"}).addClass ('table');
-	var thead = $('<thead/>').appendTo (table);
-	var tbody = $('<tbody/>', {id: options.tableId + "-tbody"}).appendTo (table);;
+	var I = sortIndexes (x.fields, "crank");
+	x.I = I;
 
+	var table = $('<table/>', {id: options.containerId + "-table"}).addClass ('table');
+
+/// CREATES HEADER
+	var thead = $('<thead/>').appendTo (table);
 	var row = $('<tr/>').appendTo (thead);
 	var c, i, j, g;
 	c = $('<th/>').appendTo (row);
 
-	var I = sortIndexes (x.fields, "crank");
 	for (i = 0; i < I.length; i ++) {
 	    c = $("<th/>").appendTo (row).text (x.fields [I [i]].label);
 	}
-	for (j = 0; j < x.rows.length; j ++) {
-	    var dataRow = x.rows [j];
-	    
-	    row = $("<tr/>", {"data-row": dataRow.id}).appendTo (tbody);
-	    c = $ ("<td/>").appendTo (row);
+	
+/// CREATES BODY AND DATA ROWS
+	var tbody = $('<tbody/>', {id: options.tableId + "-tbody"}).appendTo (table);;
+	for (j = 0; j < x.rows.length; j ++) 
+	    this.createRowElement (x.rows [j]).appendTo (tbody);
 
-	    g = createGlyph ("edit",
-			     {
-				 visible: dataRow.editable
-			     },
-			     {
-				 "data-toggle": "modal",
-				 "data-target": '#' + options.editForm.containerId
-			     })
-		.click ((function (dataRow) {  // to create a closure
-		    return function () {
-			var f = $('#' + options.editForm.containerId);
-			var form = $("#" + options.editForm.containerId + "-form");
-			form.off ("submit");
-			form.submit (function (e) {
-			    $.each (x.fields,
-				    function (_, field) {
-					if (field.inputId) {
-					    dataRow.values [field.name] = $("#" + field.inputId).val ();
-					}
-				    });
-			    self.updateRow (dataRow);
-			    e.preventDefault ();
-			    $("#" + options.editForm.containerId)
-				.modal ('hide');
-			    return false;
-			});
-			
-			f.off ('show.bs.modal');
-			f.on ('show.bs.modal', function () {
-			    $.each (x.fields,
-				    function (_, field) {
-					if (field.inputId) {
-					    var value = dataRow.values [field.name];
-					    var options = dataRow.options [field.name];
-					    var nonEditable = field.noneditable || options.noneditable;
-					    var e = $("#" + field.inputId);
-					    e[0].disabled = nonEditable;
-					    e.val (value);
-					}})})}})
-			(dataRow))
-		.appendTo (c);
-	    
-	    g = createGlyph ("remove",
-			     {
-				 visible: dataRow.deletable
-			     })
-		.appendTo (c);
-
-	    for (i = 0; i < I.length; i ++) {
-		var field = x.fields [I[i]];
-		var fname = field.name;
-		var label;
-		if (field.type.indexOf ('select') === 0) {
-		    var o = $.grep (field.options,
-				    function (o,_) {
-					return o.value === dataRow.values [fname];
-				    })[0];
-		    label = o ? o.label : '?';
-		} else {
-		    label = dataRow.values [fname];
-		}
-		
-		$("<td/>", {"data-fname": fname}).appendTo (row).text (label);
-	    }
-	}
 	return table;
     },
     
